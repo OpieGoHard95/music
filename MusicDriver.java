fb74eea9287c88b65a7f8b4fc46729beb1a8895b
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-14 15:00:10 -0500

    Needed to test one parameter constructor

--- MusicDriver.java
+++ MusicDriver.java
@@ -4,5 +4,7 @@ public class MusicDriver {
 	System.out.println(myMusic.toString());
 	Music anotherMusic = new Music("Back in Black", "AC/DC", "Metal", "CD");
 	System.out.println(anotherMusic.toString());
+	Music rageAgainstTheMachine = new Music("Rage Against the Machine");
+	System.out.println(rageAgainstTheMachine.toString());
 	}
 }
